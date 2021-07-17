854efabd85d9d284f32f82b5fc9994aff4772d8d
bahnaa <devcristopher96@gmail.com>
2021-07-17 15:37:33 +0200

    First commit

---
+++ scripts/categories.js
@@ -0,0 +1,61 @@
+const categories = document.querySelector(".categories");
+const animals = document.getElementById("animals");
+const jobs = document.getElementById("jobs");
+const sports = document.getElementById("sports");
+const fruits = document.getElementById("fruits");
+const cars = document.getElementById("cars");
+const colors = document.getElementById("colors");
+
+const database_animals = ["Lew", "Słoń", "Pszczoła", "Komar", "Wydra", "Skunks", "Żyrafa", "Orzeł", "Orangutan", "Mysz"];
+const database_jobs = ["Strażak", "Nauczyciel", "Policjant", "Sędzia", "Prawnik", "Lekarz", "Architekt", "Programista", "Marketer", "Kasjer"];
+const database_sports = ["Siatkówka", "Football", "Poker", "Koszykówka", "Pływanie", "Tenis", "Baseball", "Kręgle", "Biegi", "Pływanie"];
+const database_food = ["Pomidor", "Malina", "Melon", "Arbuz", "Brzoskwinia", "Mandarynka", "Kapusta", "Marchewka", "Kalafior", "Seler"];
+const database_cars = ["Audi", "Mercedes", "BMW", "Maybach", "Suzuki", "Toyota", "Bugatti", "Chevrolet", "Maserati", "Lamborghini"];
+const database_colors = ["Niebieski", "Czarny", "Czerwony", "Fioletowy", "Szary", "Granatowy", "Kremowy", "Zielony", "Seledynowy", "Turkusowy"]; 
+
+function categoriesHandler(databaseChoice) {
+    categories.style.display = "none";
+    container.style.display = "block";
+    database = databaseChoice;
+    game();
+}
+
+function keyboardChoice() {
+    if (event.keyCode>=49 && event.keyCode<=54) {
+        const category = String.fromCharCode(event.keyCode);
+        switch (category) {
+            case "1":
+                categoriesHandler(database_animals);
+                break;
+            case "2":
+                categoriesHandler(database_jobs);
+                break;
+            case "3":
+                categoriesHandler(database_sports);
+                break;
+            case "4":
+                categoriesHandler(database_food);
+                break;
+            case "5":
+                categoriesHandler(database_cars);
+                break;
+            case "6":
+                categoriesHandler(database_colors);
+                break;
+        }
+    }
+}
+
+animals.addEventListener("click", categoriesHandler.bind(this, database_animals));
+
+jobs.addEventListener("click", categoriesHandler.bind(this, database_jobs));
+
+sports.addEventListener("click", categoriesHandler.bind(this, database_sports));
+
+fruits.addEventListener("click", categoriesHandler.bind(this, database_food));
+
+cars.addEventListener("click", categoriesHandler.bind(this, database_cars));
+
+colors.addEventListener("click", categoriesHandler.bind(this, database_colors));
+
+document.addEventListener("keydown",  keyboardChoice);
---
+++ scripts/game.js
@@ -0,0 +1,100 @@
+const displayPassword = document.querySelector("p");
+const usedLetters = document.querySelector("p").nextElementSibling;
+const container = document.querySelector(".container");
+const image = document.querySelector("img");
+// const leaderInput = prompt("Podaj hasło do odgadnięcia (min. 2 znaki):").trim().toUpperCase().split("");
+// const fiftyLeft = document.querySelector(".fifty");
+// const fiftyRight = document.querySelector(".fifty").nextElementSibling;
+
+function game() {
+
+    document.removeEventListener("keydown", keyboardChoice);
+    let playerGuess = "";
+    let counter = 0;
+    let helpDisplayPassword = undefined;
+
+    const databaseItemNumber = Math.floor(Math.random() * database.length);
+    const password = database[databaseItemNumber].toUpperCase().split("");
+
+    function maskingPassword() {
+        for (let i=0; i<password.length; i++) {
+            displayPassword.innerHTML += "_ ";  
+        }
+    }
+
+    maskingPassword();
+
+    // this (screenPassword()) must be below maskingPassword() in order for it to have access to created displayPassword >>
+
+    function screenPassword() {
+        helpDisplayPassword = displayPassword.innerText.toString().split(" ");
+        for(let i=0; i<helpDisplayPassword.length; i++) {
+            helpDisplayPassword[i] += " ";
+        }
+    }
+
+    screenPassword();
+
+    function checkWin() {
+        if(!displayPassword.innerText.toString().split("").includes("_")) {
+        usedLetters.style.display = "none";
+        // fiftyRight.style.display = "none";
+        // fiftyLeft.style.alignItems = "center";
+        displayPassword.innerHTML = `Gratulacje!<br>Odgadłeś hasło: <span class="guessedPassword">${database[databaseItemNumber]}</span><br>Zużyte szanse: ${counter}/9`;
+        displayPassword.style.textAlign = "center";
+        }
+    }
+
+    function checkAttempts() {
+        if(usedLetters.innerText.toString().split(" ").includes(playerGuess)) {
+        alert("Już użyłeś tej litery!");
+        return;
+        }
+        counter++;
+                usedLetters.innerHTML = `${usedLetters.innerText} ${playerGuess}`;
+                image.src = `styles/images/wisielec${counter}.png`
+                if(counter==9) {
+                usedLetters.style.display="none";
+                container.style.height = "50%";
+                displayPassword.innerHTML = `Niestety! Nie udało Ci się odgadnąć hasła: <span class="unguessedPassword">${database[databaseItemNumber]}</span>`;
+                }
+    }
+
+    function checkPlayerGuess() {
+            if(password.includes(playerGuess)) {
+            password.forEach((letter, index) => {
+                if(letter == playerGuess) {           
+                    helpDisplayPassword[index] = playerGuess;
+                    displayPassword.innerHTML = "";
+                    for (let i=0; i<helpDisplayPassword.length; i++) {
+                        displayPassword.innerHTML = displayPassword.innerHTML + helpDisplayPassword[i];
+                    }
+                }
+            });
+            } else {
+                checkAttempts();
+            }
+    }
+
+    function checkButtonHandler() {
+        // turns off the function when password is (un)guessed
+        if(!displayPassword.innerText.toString().split("").includes("_")) {return};
+    //     if(playerGuess.length!=1 || !/^[AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuVvWwQqXxYyZzŹźŻż]/.test(playerGuess)) {
+    //     playerGuess.value = "";
+    //     return;
+    // } 
+        playerGuess = playerGuess.toUpperCase();
+        checkPlayerGuess();
+        if(counter==9){return};
+        checkWin();
+    }
+
+    function keyboardPlayerGuess() {
+        if (event.keyCode>=65 && event.keyCode<=90) {
+            playerGuess = String.fromCharCode(event.keyCode);
+            checkButtonHandler();
+        }
+    }
+
+    document.addEventListener("keydown", keyboardPlayerGuess);
+};
